d1db1521af28e1da83cbc363992c1e8a9b49edce
RickQuino <rquino0001@gmail.com>
2025-12-03 12:35:25 -0800

    Fortnite

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,34 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var message="I Am A Programmer!"
+    @State private var message=""
+    @State private var imageName=""
+    @State private var imageNumber = 0
+    @State private var messageNumber = 0
     var body: some View {
-       
+        
         VStack {
             Spacer()
             
-            Image(systemName: "swift")
+            Image(imageName)
                 .resizable()
                 .scaledToFit()
-                .foregroundStyle(.orange)
-                .frame(width: 200, height: 200)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.ultraLight)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
             
             Spacer()
             
-            HStack {
-                Button("Awesome"){
-                    message="Awesome!"
+            Button("Show Message"){
+                let messages = ["You are Awesome!",
+                "You are Great!",
+                "You Are Fantastic!",
+                "Fabulous? That's You!",
+                "You Make Me Smile!",
+                "When the Genius Bar Needs Help, They Call You!"]
+                
+                message = messages[messageNumber]
+                messageNumber += 1
+                if messageNumber == messages.count {
+                    messageNumber = 0
                 }
-                            Button("Great"){
-                    message="Great!"
+                
+                imageName = "image\(imageNumber)"
+                imageNumber += 1
+                
+                if imageNumber > 9 {
+                    imageNumber = 0
                 }
+                
+                print(imageNumber)
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
-            .tint(.orange)
             
         }
         .padding()
