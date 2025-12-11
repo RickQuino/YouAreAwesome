4dcab75fb0442c4eddd43a554514ba4239b24021
RickQuino <rquino0001@gmail.com>
2025-12-11 13:51:57 -0800

    dec 9

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -6,12 +6,17 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
     @State private var message=""
     @State private var imageName=""
     @State private var lastMessageNumber = -1 // lastMessageNumber will never be -1
-    @State private var LastImageNumber = -1
+    @State private var lastImageNumber = -1
+    @State private var lastSoundNumber = -1
+    @State private var audioPlayer: AVAudioPlayer!
+    let numberOfImages = 10 // images labeled image0 - image9
+    let numberOfSounds = 6 // sounds labeled sound0 - sound5
     var body: some View {
         
         VStack {
@@ -23,6 +28,8 @@ struct ContentView: View {
                 .minimumScaleFactor(0.5)
                 .frame(height: 100)
                 .animation(.easeInOut(duration: 0.15), value: message)
+            
+            Spacer()
             
             Image(imageName)
                 .resizable()
@@ -42,32 +49,42 @@ struct ContentView: View {
                                 "Fabulous? That's You!",
                                 "You Make Me Smile!",]
                 
-                // generate a random messageNumber to use an index
-                // if messageNumber == lastMessageNumber {
-                //      keep generating a new random messageNumber
-                //      until you get a messageNumber != lastMessageNumber
-                //  set messageString to messages [messageNumber]
-                //  update the lastMessageNumber with messageNumber
+                lastMessageNumber = nonRepeatingRandom(lastNumber: lastMessageNumber, upperBound: messages.count-1)
+                message = messages[lastMessageNumber]
                 
-                var messageNumber: Int
-                repeat {
-                    messageNumber = Int.random(in: 0...messages.count-1)
-                } while messageNumber == lastMessageNumber
-                        message = messages[messageNumber]
-                    lastMessageNumber = messageNumber
-                    
-                var imageNumber = Int.random(in: 0...9)
-                while imageNumber == lastMessageNumber {
-                    imageNumber = Int.random(in: 0...9)
-                }
-                imageName = "image\(imageNumber)"
-                LastImageNumber = imageNumber
+                lastImageNumber = nonRepeatingRandom(lastNumber: lastImageNumber, upperBound: numberOfImages-1)
+                imageName = "image\(lastImageNumber)"
+                
+                lastSoundNumber = nonRepeatingRandom(lastNumber: lastImageNumber, upperBound: numberOfSounds-1)
+                playSound(soundName: "sound\(lastSoundNumber)")
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
             
         }
         .padding()
+        
+    }
+    
+    func nonRepeatingRandom(lastNumber: Int, upperBound: Int) -> Int {
+        var newNumber: Int
+        repeat {
+            newNumber = Int.random(in: 0...upperBound)
+        } while newNumber == lastNumber
+        return newNumber
+    }
+    
+    func playSound(soundName: String) {
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print("😡 Could not read file named \(soundName)")
+            return
+        }
+        do{
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        } catch {
+            print("😡 ERROR: \(error.localizedDescription) creating audioPlayer")
+        }
     }
 }
 
